66fcd5224eeea0220b60ae340b67e213be90ed7b
nwe ni naing <nweninaing896@gmail.com>
2020-10-09 10:02:56 -0400

    heroku error edit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,19 +1,13 @@
+
 class ImageUploader < CarrierWave::Uploader::Base
+ include CarrierWave::MiniMagick
+ storage :file
+ def store_dir
+   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+ end
 
-  include CarrierWave::MiniMagick
-
-  storage :file
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-
-  process resize_to_fit: [400, 250]
-
-  version :thumb do
-    process resize_to_fit: [50, 50]
-  end
-
-  
+ process resize_to_fit: [400, 250]
+ version :thumb do
+   process resize_to_fit: [50, 50]
+ end
 end
